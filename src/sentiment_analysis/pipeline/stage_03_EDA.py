fafd7f3f1d8be1825b8d1bbff33121c748affc36
rezjsh <149146755+rezjsh@users.noreply.github.com>
2025-12-18 09:17:04 +0330

    Add DataEDAPipeline class for executing exploratory data analysis

---
+++ src/sentiment_analysis/pipeline/stage_03_EDA.py
@@ -0,0 +1,46 @@
+import numpy as np
+from typing import List
+from sentiment_analysis.components.EDA import SentimentEDA
+from sentiment_analysis.config.configuration import ConfigurationManager
+from sentiment_analysis.utils.logging_setup import logger
+
+class DataEDAPipeline:
+    """
+    Pipeline stage for Exploratory Data Analysis (EDA).
+    """
+    def __init__(self, config_manager: ConfigurationManager):
+        self.config_manager = config_manager
+        
+    def run_pipeline(self,preprocessed_data):
+        """
+        Executes the EDA component.
+        
+        Args:
+            preprocessed_data: Tuple containing raw texts and labels for train, val, and test sets.
+        """
+        try:
+            logger.info("Starting Data EDA Pipeline...")
+            # 1. Get configuration
+            eda_config = self.config_manager.get_eda_config()
+            
+            # 2. Get raw data
+            raw_train_texts = preprocessed_data.raw_train_texts
+            raw_train_labels = preprocessed_data.raw_train_labels
+            raw_val_texts = preprocessed_data.raw_val_texts
+            raw_val_labels = preprocessed_data.raw_val_labels
+
+
+            # 2. Initialize and run component
+            eda_component = SentimentEDA(eda_config)
+            results = eda_component.run_full_eda(
+                raw_texts=raw_train_texts,
+                raw_labels=raw_train_labels.tolist(),
+                val_texts=raw_val_texts,
+                val_labels=raw_val_labels.tolist()
+            )
+            
+            logger.info(f"Data EDA Pipeline completed successfully. Results: {results}")
+            return results
+        except Exception as e:
+            logger.error(f"Error occurred during Data EDA Pipeline: {e}")
+            raise
