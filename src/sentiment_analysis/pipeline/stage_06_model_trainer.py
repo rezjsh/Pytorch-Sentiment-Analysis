ec94bfc32e2f62dd2ccf654b1e06e62c86323b33
rezjsh <149146755+rezjsh@users.noreply.github.com>
2025-12-18 09:18:22 +0330

    Add ModelTrainerPipeline class to execute the model training pipeline

---
+++ src/sentiment_analysis/pipeline/stage_06_model_trainer.py
@@ -0,0 +1,29 @@
+from sentiment_analysis.components.model_trainer import Trainer
+from sentiment_analysis.config.configuration import ConfigurationManager
+import torch.nn as nn
+
+class ModelTrainerPipeline:
+    def __init__(self, config: ConfigurationManager, model: nn.Module, callbacks: list = None):
+        self.config = config
+        self.callbacks = callbacks
+        self.model = model
+
+    def run_pipeline(self, train_loader, val_loader) -> Trainer:
+        """Executes the model training pipeline stage."""
+        trainer_config = self.config.get_model_trainer_config()
+        self.trainer = Trainer(
+            config=trainer_config,
+            model=self.model,
+            callbacks=self.callbacks
+        )
+
+        # Train the model
+        self.trainer.train(train_loader, val_loader)
+
+        # Plot and save history
+        self.trainer.plot_history()
+
+        # Save history to CSV
+        self.trainer.save_history_to_csv()
+        return self.trainer
+        
