50e8bd9b9da542b8f5460e688986d35eea5a92a2
rezjsh <149146755+rezjsh@users.noreply.github.com>
2025-12-18 09:16:40 +0330

    Add CallbacksPipeline to manage and execute callback configurations

---
+++ src/sentiment_analysis/pipeline/stage_05_callbacks.py
@@ -0,0 +1,20 @@
+from sentiment_analysis.components.callbacks import CallabcksManager
+from sentiment_analysis.config.configuration import ConfigurationManager
+from sentiment_analysis.utils.logging_setup import logger
+
+class CallbacksPipeline:
+    """
+    Pipeline to create and manage callbacks based on configuration.
+    """
+
+    def __init__(self, config: ConfigurationManager):
+        self.config = config
+    
+    def run_pipeline(self) -> list:
+        """Creates and returns a list of callback instances."""
+        callbacks_config = self.config.get_callbacks_config()
+        callbacks_manager = CallabcksManager(config=callbacks_config)
+        callbacks = callbacks_manager.build_callbacks()
+        logger.info("Callbacks pipeline executed successfully.")
+        return callbacks
+        
